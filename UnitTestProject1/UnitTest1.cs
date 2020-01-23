63b7e913f2f926425b1ad172c831c54230d91352
kkm1998 <50990017+kkm1998@users.noreply.github.com>
2020-01-23 02:15:34 +0100

    testy

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApp3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MainWindow mainwindow = new MainWindow();
+        [TestMethod]
+        public void Button_Cl ()
+        {   
+           
+        }
+    }
+}
